0106ca9b791488b66598ea4d594854123d83ab10
sagarnaskar7596 <sagar.naskar7596872078@gmail.com>
2025-04-20 22:09:37 +0530

    Update index.js and package.json

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
-import express from 'express'
-import dotenv from "dotenv"
-import connectDb from './Database/db.js';
-import cookieParser from 'cookie-parser';
-import cloudinary from 'cloudinary';
+import express from "express";
+import dotenv from "dotenv";
+import connectDb from "./Database/db.js";
+import cookieParser from "cookie-parser";
+import cloudinary from "cloudinary";
+import path from "path";
 
 dotenv.config();
 
 cloudinary.v2.config({
-    cloud_name : process.env.Cloud_Name,
-    api_key: process.env.Cloud_Api,
-    api_secret : process.env.Cloud_Secret,
+  cloud_name: process.env.Cloud_Name,
+  api_key: process.env.Cloud_Api,
+  api_secret: process.env.Cloud_Secret,
 });
 
-const app = express()
+const app = express();
 
-const port= process.env.PORT
+const port = process.env.PORT;
 
 //using middleware thunder client
 
@@ -26,12 +27,20 @@ app.use(cookieParser());
 // })
 
 //using routes
-import userRoutes from './Routes/userRoutes.js';
-import pinRoutes from './Routes/pinRoutes.js';
+import userRoutes from "./Routes/userRoutes.js";
+import pinRoutes from "./Routes/pinRoutes.js";
 app.use("/api/user", userRoutes);
-app.use("/api/pin",pinRoutes);
+app.use("/api/pin", pinRoutes);
 
-app.listen(port,()=>{
-    console.log(`Server is runnin on http://localhost:${port}`);
-    connectDb();
-}) 
+const __dirname = path.resolve();
+
+app.use(express.static(path.join(__dirname, "../Frontend/dist")));  // Go up one level from Backend
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../Frontend/dist/index.html")); // Correct path
+});
+
+app.listen(port, () => {
+  console.log(`Server is runnin on http://localhost:${port}`);
+  connectDb();
+});
